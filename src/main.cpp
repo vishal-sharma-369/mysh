8260662cb8bb1d193708ab0e395ecdc9758733b2
Vishal Sharma <vishalsharma34258@gmail.com>
2024-09-09 18:29:26 +0530

    Implemented the type command for system builtin commands

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,78 @@
 #include <iostream>
+#include <vector>
 #include <unordered_set>
+#include <sstream>
+// #include <cstdlib>
+#include <filesystem>
 
-int evaluate_command(std::string& command, std::unordered_set<std::string>& built_ins)
+std::vector<std::string> split_command(const std::string &message, const std::string& delim) {
+  std::vector<std::string> toks;
+  std::stringstream ss = std::stringstream{message};
+  std::string line;
+  while (getline(ss, line, *delim.begin())) {
+    toks.push_back(line);
+    ss.ignore(delim.length() - 1);
+  }
+  return toks;
+}
+
+std::string get_path(std::string command)
 {
-  if(/*command.starts_with("exit")*//*command.find("exit") == 0*/ command.compare("exit 0") == 0)
+  std::string path_env = std::getenv("PATH");
+  std::stringstream ss(path_env);
+  std::string path;
+
+  while(!ss.eof())
+  {
+    getline(ss, path, ':');
+    std::string abs_path = path + "/" + command;
+
+    if(std::filesystem::exists(abs_path))
     {
-      return 0; // Signal to successfully terminating program
+      return abs_path;
     }
-    else if(command.find("echo") == 0)
+  }
+  return "";
+}
+
+int evaluate_command(std::string& command)
+{
+  std::vector<std::string> cmd_tokens = split_command(command, " ");
+  if(cmd_tokens[0].compare("exit") == 0)
+  {
+    if(command.compare("exit 0") == 0) return 0; // Signal to successfully terminating program
+    return 1; // Unsuccessful termination of program
+  }
+  else if(cmd_tokens[0].compare("echo") == 0)
+  {
+    std::cout<<command.substr(5)<<std::endl;
+    return -1; // Signal for successful execution of command and continue to next loop
+  }
+  else if(cmd_tokens[0].compare("type") == 0)
+  {
+    std::string path = get_path(cmd_tokens[1]);
+
+    if(path.empty())
     {
-      std::cout<<command.substr(5)<<std::endl;
-      return -1; // Signal for successful execution of command and continue to next loop
+      std::cout<<cmd_tokens[1]<<": not found"<<std::endl;
     }
     else
     {
-      std::cout<<command<<": command not found"<<std::endl;
-      return -1;
+      std::cout<<cmd_tokens[1]<<" is "<<path<<std::endl;
     }
+    return -1;
+  }
+  else
+  {
+    std::cout<<command<<": command not found"<<std::endl;
+    return -1;
+  }
 }
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
-  // Defining the built-in commands
-  std::unordered_set<std::string> built_ins={"echo","exit","type"};
 
   // (REPL Loop)
   while(true)
@@ -36,7 +83,7 @@ int main() {
     std::getline(std::cin, input);
 
     // Evaluate command and Print output
-    int result = evaluate_command(input, built_ins);
+    int result = evaluate_command(input);
     if(result == 0) return 0;
   }
   return 0;
