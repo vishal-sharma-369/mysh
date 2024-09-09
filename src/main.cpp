e91c9f3c30ea326aa7f87f2e21d0095439f38618
Vishal Sharma <vishalsharma34258@gmail.com>
2024-09-09 11:45:33 +0530

    Implemented REPL(Read Evaluate Print Loop) Functionality

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,11 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
-
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout<<input<<": command not found"<<std::endl;
+  while(true)
+  {
+    std::cout << "$ ";
+    std::string input;
+    std::getline(std::cin, input);
+    std::cout<<input<<": command not found"<<std::endl;
+  }
 }
